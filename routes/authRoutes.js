3aaa09a732df852bce6e11e8a436850f78b69bf0
josesantana8929-ux <josesantana.8929@gmail.com>
2026-04-07 21:59:23 -0400

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/authController');  // ✅ Debe existir
 const { authenticateToken } = require('../utils/jwtUtils');
 const { validateRegister, validateLogin, validateProfileUpdate } = require('../middleware/validators');
 
